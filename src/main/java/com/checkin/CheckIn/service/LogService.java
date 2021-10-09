034f02ad58268a1ea8fe25fec9ba89b7f588f511
ghkdwlgns612 <ghkdwlgns612@naver.com>
2021-10-09 15:56:33 +0900

    LogService로직 작성완료

--- src/main/java/com/checkin/CheckIn/service/LogService.java
+++ src/main/java/com/checkin/CheckIn/service/LogService.java
@@ -1,4 +1,73 @@
 package com.checkin.CheckIn.service;
 
+import com.checkin.CheckIn.domain.User;
+import com.checkin.CheckIn.repository.UserRepository;
+import com.checkin.CheckIn.service.dto.LocationDto;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Slf4j
+@AllArgsConstructor
 public class LogService {
+    private final UserRepository userRepository;
+
+    public List<LocationDto> usingCardGeapo() {
+        List<User> users = userRepository.findByCardNumberGeapo();
+        List<LocationDto> result = new ArrayList<>();
+        users.stream().forEach(user -> {
+            LocationDto locationDto = LocationDto.builder()
+                    .cardNumber(user.getCardNumber())
+                    .createTime(user.getCreated())
+                    .location("Geapo")
+                    .username(user.getUsername())
+                    .build();
+            result.add(locationDto);
+        });
+        return result;
+    }
+
+    public List<LocationDto> usingCardSeocho() {
+        List<User> users = userRepository.findByCardNumberSeocho();
+        List<LocationDto> result = new ArrayList<>();
+        users.stream().forEach(user -> {
+            LocationDto locationDto = LocationDto.builder()
+                    .cardNumber(user.getCardNumber())
+                    .createTime(user.getCreated())
+                    .location("Seocho")
+                    .username(user.getUsername())
+                    .build();
+            result.add(locationDto);
+        });
+        return result;
+    }
+
+    public List<LocationDto> usingCardAll() {
+        List<User> users = userRepository.findAll();
+        List<LocationDto> result = new ArrayList<>();
+        users.stream().forEach(user -> {
+            if (user.getCardNumber() < 1000) {
+                LocationDto locationDto = LocationDto.builder()
+                        .cardNumber(user.getCardNumber())
+                        .createTime(user.getCreated())
+                        .location("Geapo")
+                        .username(user.getUsername())
+                        .build();
+                result.add(locationDto);
+            } else {
+                LocationDto locationDto = LocationDto.builder()
+                        .cardNumber(user.getCardNumber())
+                        .createTime(user.getCreated())
+                        .location("Seocho")
+                        .username(user.getUsername())
+                        .build();
+                result.add(locationDto);
+            }
+        });
+        return result;
+    }
 }
