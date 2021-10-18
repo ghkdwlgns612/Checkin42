f37705b2e02dd2f05e745da535dd299138d594f5
ghkdwlgns612 <ghkdwlgns612@naver.com>
2021-10-18 12:39:38 +0900

    CHKN2-3 GlobalExceptionHandler추가(모든 Exception처리역할)

---
+++ src/main/java/com/checkin/CheckIn/error/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.checkin.CheckIn.error;
+
+import com.checkin.CheckIn.domain.ResultResponseDto;
+import javassist.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResultResponseDto handlerNotFoundException(Exception e) {
+        log.info(e.getMessage());
+        return ResultResponseDto.builder()
+                .statusCode(HttpStatus.NOT_FOUND.value())
+                .message(e.getMessage())
+                .data(null)
+                .build();
+    }
+}
