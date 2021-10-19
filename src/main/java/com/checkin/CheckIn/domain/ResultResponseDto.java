ea7338888deadd5154a7d26ab8f98940c13cf5c9
ghkdwlgns612 <ghkdwlgns612@naver.com>
2021-10-19 16:41:05 +0900

    CHKN2-17 Response설명추가.

--- src/main/java/com/checkin/CheckIn/domain/ResultResponseDto.java
+++ src/main/java/com/checkin/CheckIn/domain/ResultResponseDto.java
@@ -1,14 +1,19 @@
 package com.checkin.CheckIn.domain;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@ApiModel(value = "Result")
 public class ResultResponseDto<T> {
 
+    @ApiModelProperty(value = "상태코드")
     private Integer statusCode;
+    @ApiModelProperty(value = "상태메세지")
     private String message;
     private T data;
 
