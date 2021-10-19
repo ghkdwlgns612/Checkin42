f19eb12e25800bdc4c0e95493d7b12bbd796c303
ParkGunWook <saiver852@gmail.com>
2021-10-19 21:41:09 +0900

    CHKN2-16 Replace to Mysql Dependency, yml data source, add sql init

---
+++ src/main/resources/db/schema.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS USER CASCADE;
+
+CREATE TABLE USER (
+                      id              BIGINT          PRIMARY KEY AUTO_INCREMENT,
+                      intra_id        BIGINT          NOT NULL,
+                      username        VARCHAR(255)    NOT NULL,
+                      card_number     SMALLINT        UNIQUE,
+                      check_in        DATETIME        DEFAULT NOW(),
+                      check_out       DATETIME        DEFAULT NOW()
+);
