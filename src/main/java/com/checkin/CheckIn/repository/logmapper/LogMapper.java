3ecdf7eea43c36dffc021a4d480d5e18e9b324fe
ghkdwlgns612 <ghkdwlgns612@naver.com>
2021-11-03 14:24:43 +0900

    CHKN2-32 Log 기본 메소드 추가.

--- src/main/java/com/checkin/CheckIn/repository/logmapper/LogMapper.java
+++ src/main/java/com/checkin/CheckIn/repository/logmapper/LogMapper.java
@@ -3,8 +3,15 @@ package com.checkin.CheckIn.repository.logmapper;
 import com.checkin.CheckIn.domain.Log;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+import java.util.Optional;
+
 @Mapper
 public interface LogMapper {
     void save(Log log);
+
+    Optional<Log> findByName(String username);
+
+    List<Log> findAll();
 }
 
