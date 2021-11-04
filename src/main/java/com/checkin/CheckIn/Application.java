db0c835eb37ec3c70098402dbbe2c9780c78b5f7
ParkGunWook <saiver852@gmail.com>
2021-11-04 16:28:26 +0900

    Revert "CHKN2-26 EmbeddedTomcat 80포트 진입에 대한 443 포트 자동 redirect(임시)"
    This reverts commit 8669bfec9e48b418cd03efb7654ac7cddbb9d23f.

--- src/main/java/com/checkin/CheckIn/Application.java
+++ src/main/java/com/checkin/CheckIn/Application.java
@@ -1,16 +1,9 @@
 package com.checkin.CheckIn;
 
 import com.checkin.CheckIn.utils.resource.YAMLSecurityResource;
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties(YAMLSecurityResource.class)
@@ -20,30 +13,4 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Bean
-	public ServletWebServerFactory servletContainer() {
-		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-			@Override
-			protected void postProcessContext(Context context) {
-				SecurityConstraint securityConstraint = new SecurityConstraint();
-				securityConstraint.setUserConstraint("CONFIDENTIAL");
-				SecurityCollection collection = new SecurityCollection();
-				collection.addPattern("/path/*");
-				securityConstraint.addCollection(collection);
-				context.addConstraint(securityConstraint);
-			}
-		};
-
-		tomcat.addAdditionalTomcatConnectors(createSslConnector());
-		return tomcat;
-	}
-
-	private Connector createSslConnector() {
-		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-		connector.setPort(80);
-		connector.setScheme("http");
-		connector.setSecure(false);
-		connector.setRedirectPort(443);
-		return connector;
-	}
 }
