d4bd70f2b935bd3b7362b4c7f61cc61cdc9b2733
ParkGunWook <saiver852@gmail.com>
2021-11-10 14:02:31 +0900

    CHKN2-19 쿠키관련 코드 리팩토링

--- src/main/java/com/checkin/CheckIn/controller/CallBackController.java
+++ src/main/java/com/checkin/CheckIn/controller/CallBackController.java
@@ -3,7 +3,9 @@ package com.checkin.CheckIn.controller;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.checkin.CheckIn.domain.ResultResponseDto;
+import com.checkin.CheckIn.domain.User;
 import com.checkin.CheckIn.repository.usermapper.UserMapper;
+import com.checkin.CheckIn.utils.CookieUtils;
 import com.checkin.CheckIn.utils.JWTUtils;
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
@@ -27,55 +29,33 @@ import java.util.Optional;
 public class CallBackController {
 
     private final JWTUtils jwtUtils;
+    private final CookieUtils cookieUtils;
     private final UserMapper userMapper;
 
-    public CallBackController(JWTUtils jwtUtils, UserMapper userMapper) {
+    public CallBackController(JWTUtils jwtUtils, CookieUtils cookieUtils, UserMapper userMapper) {
         this.jwtUtils = jwtUtils;
+        this.cookieUtils = cookieUtils;
         this.userMapper = userMapper;
+    }
+
+    @GetMapping("user/login/callback/42")
+    public void validateLogin(@RequestParam String code) {
+
     }
 
     @GetMapping("/mock-make-token/{username}")
     @Operation(summary = "쿠키 자동 설정", description = "username으로 JWT를 만들고 set-Cookie를 통해서 \"/\" path에 쿠키를 자동 세팅해줍니다.")
     public ResultResponseDto<String> MockMakeToken(@PathVariable String username, HttpServletResponse response, HttpServletRequest request) {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        if (userMapper.findByName(username).isPresent()) {
-            Cookie cookie = new Cookie("basictoken", jwtUtils.makeJWT(userMapper.findByName(username).get()));
-            cookie.setMaxAge(7 * 24 * 60 * 60);
-            cookie.setPath("/");
-            cookie.setDomain("42cadet.kr");
-            Cookie cookie1 = new Cookie("localtoken", jwtUtils.makeJWT(userMapper.findByName(username).get()));
-            cookie1.setMaxAge(7 * 24 * 60 * 60);
-            cookie1.setPath("/");
-            cookie1.setDomain("localhost");
-            response.addCookie(cookie);
-            response.addCookie(cookie1);
-            ResponseCookie responseCookie = ResponseCookie.from("httpsNoneToken", jwtUtils.makeJWT(userMapper.findByName(username).get()))
-                    .domain("42cadet.kr")
-                    .secure(true)
-                    .sameSite("None")
-                    .maxAge(7 * 24 * 60 * 60)
-                    .build();
-            ResponseCookie responseCookie1 = ResponseCookie.from("httpsLaxToken", jwtUtils.makeJWT(userMapper.findByName(username).get()))
-                    .domain("42cadet.kr")
-                    .secure(true)
-                    .sameSite("Lax")
-                    .maxAge(7 * 24 * 60 * 60)
-                    .build();
-            ResponseCookie responseCookie2 = ResponseCookie.from("httpsStrictToken", jwtUtils.makeJWT(userMapper.findByName(username).get()))
-                    .domain("42cadet.kr")
-                    .secure(true)
-                    .sameSite("Strict")
-                    .maxAge(7 * 24 * 60 * 60)
-                    .build();
-            response.addHeader("Set-Cookie", responseCookie.toString());
-            response.addHeader("Set-Cookie", responseCookie1.toString());
-            response.addHeader("Set-Cookie", responseCookie2.toString());
+        try {
+            User user = userMapper.findByName(username).orElseThrow();
+            Arrays.stream(cookieUtils.makeAllConditionCookie(user))
+                    .forEach(responseCookie -> response.addHeader("Set-cookie", responseCookie.toString()));
             return ResultResponseDto.<String>builder()
                     .statusCode(HttpStatus.OK.value())
                     .message("4 option cookie Setting")
                     .data(username + "'s token use https://www.base64decode.org/")
                     .build();
-        } else {
+        } catch (Exception e) {
             return ResultResponseDto.<String>builder()
                     .statusCode(HttpStatus.BAD_REQUEST.value())
                     .message("No such user exists")
