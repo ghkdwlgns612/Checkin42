72b4fb317ed5a88ebcd996b5b8b591cd3184c693
ParkGunWook <saiver852@gmail.com>
2021-11-10 14:05:17 +0900

    CHKN2-19 Oauth resource와 JWT resource 분리

--- src/main/java/com/checkin/CheckIn/Application.java
+++ src/main/java/com/checkin/CheckIn/Application.java
@@ -1,12 +1,12 @@
 package com.checkin.CheckIn;
 
-import com.checkin.CheckIn.utils.resource.YAMLSecurityResource;
+import com.checkin.CheckIn.utils.resource.JWTSecurityResource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(YAMLSecurityResource.class)
+@EnableConfigurationProperties({JWTSecurityResource.class})
 public class Application {
 
 	public static void main(String[] args) {
