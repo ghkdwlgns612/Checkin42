a5661cf8dcc0ae42823c6cfe18768c070d53b9da
ParkGunWook <saiver852@gmail.com>
2021-11-13 12:11:19 +0900

    CHKN2-19 기본적인 Security 설정을 통한 필터 작동 확인 및 JWT 필터 설정

---
+++ src/main/java/com/checkin/CheckIn/config/SecurityConfig.java
@@ -0,0 +1,72 @@
+package com.checkin.CheckIn.config;
+
+import com.checkin.CheckIn.service.OAuth42UserService;
+import com.checkin.CheckIn.utils.resource.CustomOAuth2Provider;
+import com.checkin.CheckIn.utils.resource.OAuth42Resource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+
+import javax.servlet.Filter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.security.oauth2.core.AuthorizationGrantType.AUTHORIZATION_CODE;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final OAuth42Resource oAuth42Resource;
+    private final OAuth42UserService oAuth42UserService;
+
+    public SecurityConfig(OAuth42Resource oAuth42Resource, OAuth42UserService oAuth42UserService) {
+        this.oAuth42Resource = oAuth42Resource;
+        this.oAuth42UserService = oAuth42UserService;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        Filter jwtFilter = new JWTFilter();
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeRequests(expressionInterceptUrlRegistry ->
+                        expressionInterceptUrlRegistry
+                                .antMatchers("/cluster", "/swagger-ui/**/", "/login/callback/**", "/user/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .sessionManagement(httpSecuritySessionManagementConfigurer ->
+                        httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        ;
+        http.addFilterAfter(jwtFilter, LogoutFilter.class);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/swagger-resources/**", "/v2/api-docs");
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        List<ClientRegistration> registrationList = new ArrayList<>();
+        registrationList.add(CustomOAuth2Provider.FOURTYTWO.getBuilder("42")
+                .clientId(oAuth42Resource.getClientId())
+                .clientSecret(oAuth42Resource.getClientSecret())
+                .redirectUri(oAuth42Resource.getRedirectUri())
+                .authorizationGrantType(AUTHORIZATION_CODE).build());
+        return new InMemoryClientRegistrationRepository(registrationList);
+    }
+}
