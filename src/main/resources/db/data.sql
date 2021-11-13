413c795b88b7f2b7a2005077c4eef8c7d8ae0c35
ParkGunWook <saiver852@gmail.com>
2021-11-13 21:51:24 +0900

    CHKN2-48 기본 데이터 변경에 따른 통합 테스트 조건 수정.

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -2,7 +2,7 @@
 # insert into USER(intra_id , username, card_number, check_in, check_out) values(86991, "jihuhwan", 13, "2021-10-01 13:00:00",  "2021-10-01 20:00:00");
 # insert into USER(intra_id , username, card_number, check_in, check_out) values(86933, "yunjung", 14, "2021-10-01 10:00:00",  "2021-10-01 23:59:59");
 # insert into USER(intra_id , username, card_number, check_in, check_out) values(86982, "minjupar", 15, "2021-10-01 09:00:00",  "2021-10-01 22:00:00");
-insert into USER(intra_id , username, card_number, check_in, check_out) values(86111, "gpark1", 1012,  "2021-10-01 09:00:00",  "2021-10-01 12:00:00");
-insert into USER(intra_id , username, card_number, check_in, check_out) values(86222, "jihuhwan1", 1013, "2021-10-01 13:00:00",  "2021-10-01 20:00:00");
-insert into USER(intra_id , username, card_number, check_in, check_out) values(86333, "yunjung1", 1014, "2021-10-01 10:00:00",  "2021-10-01 23:59:59");
-insert into USER(intra_id , username, card_number, check_in, check_out) values(86444, "minjupar1", 1015, "2021-10-01 09:00:00",  "2021-10-01 22:00:00");
+insert into USER(intra_id , username, card_number, check_in, check_out) values(86111, "gpark1", null,  "2021-10-01 09:00:00",  "2021-10-01 12:00:00");
+insert into USER(intra_id , username, card_number, check_in, check_out) values(86222, "jihuhwan1", null, "2021-10-01 13:00:00",  "2021-10-01 20:00:00");
+insert into USER(intra_id , username, card_number, check_in, check_out) values(86333, "yunjung1", null, "2021-10-01 10:00:00",  "2021-10-01 23:59:59");
+insert into USER(intra_id , username, card_number, check_in, check_out) values(86444, "minjupar1", null, "2021-10-01 09:00:00",  "2021-10-01 22:00:00");
